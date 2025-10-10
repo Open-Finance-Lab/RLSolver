91909f88df09ac8fc6188bf27901f568a0b78ebf
Nathan_mks <148317944+moimksa@users.noreply.github.com>
2025-10-11 00:53:09 +0800

    Update _env_base.py

--- rlsolver/envs/_env_base.py
+++ rlsolver/envs/_env_base.py
@@ -1,35 +1,33 @@
-from abc import ABC,abstractmethod
-import gymnasium as gym
+import torch
+from abc import ABC, abstractmethod
 
 
-class _EnvBase(ABC, gym.Wrapper):
-    def __init__(self, num_envs: int, state_dim: int, action_dim: int, if_discrete: bool):
+class EnvBase(ABC):
+    def __init__(self, num_envs, state_dim, action_dim, if_discrete, device):
         self.num_envs = num_envs
         self.state_dim = state_dim
         self.action_dim = action_dim
         self.if_discrete = if_discrete
-
+        self.device = device
+    
     @abstractmethod
     def reset(self):
         pass
-
+    
+    @abstractmethod
+    def step(self, action):
+        pass
+    
     @abstractmethod
     def reward(self):
         pass
-
-    @abstractmethod
-    def step(self, state, action):
-        pass
-
-    @abstractmethod
+    
     def reset_parallel(self):
-        pass
-
-    @abstractmethod
-    def reward_parallel(self):
-        pass
-
-    @abstractmethod
-    def step_parallel(self, states, actions):
-        pass
-
+        dummy = torch.zeros(self.num_envs, device=self.device)
+        return torch.vmap(lambda _: self.reset())(dummy)
+    
+    def step_parallel(self, actions):
+        return torch.vmap(self.step)(actions)
+    
+    def reward_parallel(self, tours):
+        return torch.vmap(self.reward)(tours)
